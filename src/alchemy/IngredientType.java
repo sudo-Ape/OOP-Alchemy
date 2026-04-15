710e89ec6dc03b02cb4e1ad7628ba670e2045f8a
CasperThePancake <60041702+CasperThePancake@users.noreply.github.com>
2026-04-15 19:38:43 +0200

    Finished ingredientType, except for proper basic ingredient name checks since they are complex

--- src/alchemy/IngredientType.java
+++ src/alchemy/IngredientType.java
@@ -1,4 +1,147 @@
 package alchemy;
 
+import java.util.*;
+
 public class IngredientType {
+    // =================================================================================
+    // Attributes
+    // =================================================================================
+
+    private int standardTemperature;
+    private State standardState;
+    private Set<String> basicIngredients = new HashSet<>(); // Set to ensure no duplicate basic ingredients (possibly caused by mixing)
+
+    // =================================================================================
+    // Constructors
+    // =================================================================================
+    /**
+     * Create a new ingredientType with given standard temperature, given standard state, and given list of basic ingredient names
+     *
+     * @param standardTemperature Given standard temperature
+     * @param standardState Given standard state
+     * @param basicIngredients Given basic ingredient names
+     *
+     * @note This constructor is package-private: the user cannot use it, only other code can!
+     */
+    IngredientType(int standardTemperature, State standardState, Set<String> basicIngredients) {
+        this.standardTemperature = standardTemperature;
+        this.standardState = standardState;
+        this.basicIngredients = basicIngredients;
+    }
+
+    // =================================================================================
+    // Getters
+    // =================================================================================
+    /**
+     * Get the standard temperature for this ingredient type
+     *
+     * @return Standard temperature for this ingredient type
+     */
+    public int getStandardTemperature() {
+        return standardTemperature;
+    }
+
+    /**
+     * Get the standard state for this ingredient type
+     *
+     * @return Standard state for this ingredient type
+     */
+    public State getStandardState() {
+        return standardState;
+    }
+
+    /**
+     * Get the basic ingredients list for this ingredient type
+     *
+     * @return Basic ingredients list for this ingredient type
+     */
+    private Set<String> getBasicIngredients() {
+        return basicIngredients;
+    }
+
+    // =================================================================================
+    // Setters
+    // =================================================================================
+
+    /**
+     * Set the basic ingredients for this ingredient type
+     *
+     * @pre The given set of basic ingredients is allowed
+     *      | canHaveAsBasicIngredients(basicIngredients)
+     *
+     * @param basicIngredients Given set of basic ingredients
+     */
+    public void setBasicIngredients(Set<String> basicIngredients) {
+        this.basicIngredients = basicIngredients;
+    }
+
+    // =================================================================================
+    // Inspectors
+    // =================================================================================
+
+    /**
+     * Check whether given list of basic ingredients is allowed
+     *
+     * @param basicIngredients Given list of basic ingredients to check
+     *
+     * @return Whether this list of basic ingredients is allowed
+     */
+    private boolean canHaveAsBasicIngredients(Set<String> basicIngredients) {
+        for (String ingredient : basicIngredients) {
+            if (!canHaveAsBasicIngredient(ingredient)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Check if given basic ingredient name is allowed name
+     *
+     * @param basicIngredient Basic ingredient name to check
+     *
+     * @return Whether this basic ingredient name is allowed
+     *
+     * @note Not implemented yet! Complex!
+     */
+    private boolean canHaveAsBasicIngredient(String basicIngredient) {
+        return true;
+    }
+
+    // =================================================================================
+    // Simple name
+    // =================================================================================
+    /**
+     * Get the simple name for this ingredient type
+     *
+     * @return Simple name for this ingredient type
+     */
+    public String getSimpleName() {
+        String output = "";
+
+        // Convert set to sorted list
+        List<String> basicIngredients = new ArrayList<>(getBasicIngredients());
+        basicIngredients.sort(Comparator.naturalOrder());
+
+        int N = basicIngredients.size();
+        for (int i = 0; i < N; i++) {
+            output += basicIngredients.get(i);
+
+            // Check if "mixed with" is necessary
+            if (i == 0 && N > 1) {
+                output.join(" mixed with ");
+            }
+
+            // Check if "and" or "," is necessary
+            if (i > 0 && i < N-1) {
+                if (i == N-2) {
+                    output.join(" and ");
+                } else {
+                    output.join(", ");
+                }
+            }
+        }
+
+        return output;
+    }
 }
