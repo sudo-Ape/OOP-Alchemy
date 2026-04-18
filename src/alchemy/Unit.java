e871783c1795da7fdf7c85e7528d081958068a6a
CasperThePancake <60041702+CasperThePancake@users.noreply.github.com>
2026-04-18 13:02:58 +0200

    Fixed Loïck shit

--- src/alchemy/Unit.java
+++ src/alchemy/Unit.java
@@ -4,42 +4,39 @@ import be.kuleuven.cs.som.annotate.Basic;
 import be.kuleuven.cs.som.annotate.Raw;
 
 public enum Unit {
-    DROP("drop"),
-    SPOON("spoon"),         // 6 drops (L), 6 pinches (P)
-    VIAL("vial"),           // 5 spoons (L)
-    BOTTLE("bottle"),       // 3 vials (L)
-    JUG("jug"),             // 7 bottles (L)
-    BARREL("barrel"),       // 12 jugs (L)
-    STOREROOM("storeroom"), // 5 barrels (L), 5 chests (P)
+    DROP(1/8),
+    SPOON(1),
+    VIAL(5),
+    BOTTLE(15),
+    JUG(105),
+    BARREL(1260),
+    STOREROOM(6300),
 
-    PINCH("pinch"),
-    SACHET("sachet"),       // 7 spoons (P)
-    BOX("box"),             // 6 sachets (P)
-    SACK("sack"),           // 3 boxes
-    CHEST("chest");         // 10 sacks
+    PINCH(1/6),
+    SACHET(7),
+    BOX(42),
+    SACK(126),
+    CHEST(1260);
 
-    private String unit = null;
+    private float spoons;
 
     /**
      * Initialize a new ingredient with given unit
      *
-     * @param unit
-     *        The unit of the ingredient
+     * @param spoons The amount of spoons this unit is equal to
      *
      * @note The unit will depend on the state of the ingredient (L or P)
      */
-    Unit(String unit) {
-        this.unit = unit;
+    Unit(float spoons) {
+        this.spoons = spoons;
     }
 
     /**
-     * Returns the unit of the ingredient
+     * Get the amount of spoons this unit is equal to
      *
-     * @return the unit if the ingredient
+     * @return Amount of spoons this unit is equal to
      */
-    @Basic
-    @Raw
-    public String getUnit() {
-        return unit;
+    public float getSpoons() {
+        return spoons;
     }
 }
